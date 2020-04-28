fb938036e29825573c269e806f348485d83e174d
soffet713 <30372648+soffet713@users.noreply.github.com>
2020-04-28 13:49:21 -0400

    Update FunctionsPage.cs

--- FunctionsPage.cs
+++ FunctionsPage.cs
@@ -1,7 +1,5 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
